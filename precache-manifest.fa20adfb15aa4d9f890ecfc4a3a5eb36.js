47e16e8e90733637b5c37b2778ecf54a9889b9dc
Mariia Synoverska <mariykasinover@gmail.com>
2019-01-01 22:22:45 +0200

    Updates

---
+++ precache-manifest.fa20adfb15aa4d9f890ecfc4a3a5eb36.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "46c0bd59adb9b6ca394698fd6169b290",
+    "url": "/react-redux-blog/static/media/fb-icon.46c0bd59.svg"
+  },
+  {
+    "revision": "44e3ba26c66dbb18ef63",
+    "url": "/react-redux-blog/static/css/main.a298dd23.chunk.css"
+  },
+  {
+    "revision": "a92609e1d2282eefeaf0",
+    "url": "/react-redux-blog/static/js/1.a92609e1.chunk.js"
+  },
+  {
+    "revision": "a304f620eb9a1d70514f",
+    "url": "/react-redux-blog/static/js/runtime~main.a304f620.js"
+  },
+  {
+    "revision": "56891f3403aecd6a4572ec1ccc4e4fdb",
+    "url": "/react-redux-blog/static/media/itemBg.56891f34.jpg"
+  },
+  {
+    "revision": "44e3ba26c66dbb18ef63",
+    "url": "/react-redux-blog/static/js/main.44e3ba26.chunk.js"
+  },
+  {
+    "revision": "3ee3ca382ffca890ed37a4b96d876520",
+    "url": "/react-redux-blog/static/media/twit-icon.3ee3ca38.svg"
+  },
+  {
+    "revision": "8dc04bcddc1ee128393621e994aa53e9",
+    "url": "/react-redux-blog/static/media/link-icon.8dc04bcd.svg"
+  },
+  {
+    "revision": "58de3086d267f2f582930c027dffd60e",
+    "url": "/react-redux-blog/static/media/search-icon.58de3086.svg"
+  },
+  {
+    "revision": "428b5e9136475d55bb7acfe52ac70af2",
+    "url": "/react-redux-blog/static/media/icon-load.428b5e91.svg"
+  },
+  {
+    "revision": "ff9339a95885b536593a54a82703c001",
+    "url": "/react-redux-blog/index.html"
+  }
+];
